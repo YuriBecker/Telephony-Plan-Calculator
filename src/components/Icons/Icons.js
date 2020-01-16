1d6f71143ef94294cd723088d02187ac1acd4d5a
Yuri Becker <becker.yr@gmail.com>
2020-01-16 16:57:22 -0300

    Update Icons component

--- src/components/Icons/Icons.js
+++ src/components/Icons/Icons.js
@@ -4,3 +4,5 @@ import { Icon } from '@ui-kitten/components';
 export const NextIcon = style => (
   <Icon name="arrow-forward-outline" {...style} />
 );
+
+export const PhoneIcon = style => <Icon name="phone-outline" {...style} />;
