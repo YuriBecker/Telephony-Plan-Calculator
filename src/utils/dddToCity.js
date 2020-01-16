6147113f1896d3f0748096f10a4836cac8128ad8
Yuri Becker <becker.yr@gmail.com>
2020-01-16 17:01:53 -0300

    Remove funct

--- src/utils/dddToCity.js
+++
@@ -1,9 +0,0 @@
-import dddList from '../constants/ddd';
-
-const dddToCity = cityDDD => {
-  if (Object.prototype.hasOwnProperty.call(dddList, cityDDD))
-    return dddList[cityDDD];
-  return 'City not found';
-};
-
-export default dddToCity;
