b430afca2b1d28e61db669c59cb2a26a6a872187
Yuri Becker <becker.yr@gmail.com>
2020-01-17 16:47:15 -0300

    Update navigation

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,18 +1,14 @@
-import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
 import IntroApp from '../screens/IntroApp';
 import Result from '../screens/Result';
-import colors from '../styles/colors';
 import StepForm from '../screens/StepForm';
-import FaleMaisLogo from '../components/FaleMaisLogo';
 
 const AppNavigator = createSwitchNavigator(
   {
     SliderIntro: {
       screen: IntroApp,
     },
-    MainApp: createStackNavigator(
+    MainApp: createSwitchNavigator(
       {
         Form: {
           screen: StepForm,
@@ -22,19 +18,7 @@ const AppNavigator = createSwitchNavigator(
         },
       },
       {
-        defaultNavigationOptions: {
-          headerTintColor: colors.primary,
-          headerStyle: {
-            backgroundColor: colors.black[1],
-          },
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-          headerTitleAlign: 'center',
-          headerTitle: () => (
-            <FaleMaisLogo titleCategory="h4" iconHeight={28} iconWidth={28} />
-          ),
-        },
+        initialRouteName: 'Form',
       }
     ),
   },
